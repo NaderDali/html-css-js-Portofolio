3c385d34871265e023eeba7901a9333ca3c8627d
NaderDali <46582693+NaderDali@users.noreply.github.com>
2024-02-02 16:09:55 +0200

    Add files via upload
    CRT Car Rental Test

--- Home.jsx
+++ Home.jsx
@@ -1,30 +1,26 @@
-import Banner from "../components/Banner";
-import BannerReverse from "../components/BannerReverse";
-import Footer from "../components/Footer";
 import Hero from "../components/Hero";
-import Newsletter from "../components/Newsletter";
-import ProudProducts from "../components/ProudProducts";
-import TrendingSlider from "../components/TrendingSlider";
-import Banner1 from "../img/banner/banner1.jpg";
-import Banner2 from "../img/banner/banner2.jpg";
+import BookCar from "../components/BookCar";
+import PlanTrip from "../components/PlanTrip";
+import PickCar from "../components/PickCar";
+import Banner from "../components/Banner";
+import ChooseUs from "../components/ChooseUs";
+import Testimonials from "../components/Testimonials";
+import Faq from "../components/Faq";
+import Download from "../components/Download";
+import Footer from "../components/Footer";
 
 function Home() {
   return (
     <>
       <Hero />
-      <ProudProducts />
-      <Banner
-        title="Creative harmonious living"
-        text=" RAOUF Products are all made to standard sizes so that you can mix and match them freely."
-        img={Banner1}
-      />
-      <TrendingSlider />
-      <BannerReverse
-        title="Comfortable & Elegante Living"
-        text=" RAOUF Products are all made to standard sizes so that you can mix and match them freely."
-        img={Banner2}
-      />
-      <Newsletter />
+      <BookCar />
+      <PlanTrip />
+      <PickCar />
+      <Banner />
+      <ChooseUs />
+      <Testimonials />
+      <Faq />
+      <Download />
       <Footer />
     </>
   );
